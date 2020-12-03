bace718a1f18ed95afe5291d9dae442e4b567bf5
haruu11113 <haruu11113@gmail.com>
2020-12-04 02:04:35 +0900

    feat(save csv):

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import socket
 
+with open ('./data/data.csv', 'r') as f:
+    print(f.read())
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind(('192.168.10.200', 6666))
     while True:
